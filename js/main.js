cdcafedebced80fd431a5d89ad8ea621e9bb5cd8
irinaponomarenko <ponomarenko02041993@gmail.com>
2021-04-28 23:31:22 +0300

    media

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $( document ).ready(function() {
    //-------------dropdown language------------//
     let nav = $('.lang');
-    let selection = $('.list-language');
+    let nav2 = $('.lang2');
+    let selection = $('#langList');
+    let selection2 = $('#langList2');
     let select = selection.find('.item-language');
+    let select2 = selection2.find('.item-language');
 
     nav.click(function(event) {
         if (nav.hasClass('active-lng')) {
@@ -15,12 +18,31 @@ $( document ).ready(function() {
         event.preventDefault();
     });
 
+    nav2.click(function(event) {
+        if (nav2.hasClass('active-lng')) {
+            nav2.removeClass('active-lng');
+            selection2.stop().slideUp(200);
+        } else {
+            nav2.addClass('active-lng');
+            selection2.stop().slideDown(200);
+        }
+        event.preventDefault();
+    });
+
     select.click(function(event) {
         select.removeClass('active-lng');
         $(this).addClass('active-lng');
         let $lang =  $(this).text();
         nav.text($lang);
         nav.trigger('click');
+    });
+
+    select2.click(function(event) {
+        select2.removeClass('active-lng');
+        $(this).addClass('active-lng');
+        let $lang2 =  $(this).text();
+        nav2.text($lang2);
+        nav2.trigger('click');
     });
 
 
